3f80099b99e69ac2b49b5ffd6185690b386ca3e1
rpsh <myrpsh@gmail.com>
2022-03-15 00:01:02 +0800

    feat: readme 中往期目录修改为时间倒序

--- readme.js
+++ readme.js
@@ -22,6 +22,7 @@ function getVols() {
   // 按发布日期排序处理一下
   const orderedVols = Object.keys(vols)
     .sort()
+    .reverse() // 时间倒序
     .reduce((obj, key) => {
       obj[key] = vols[key];
       return obj;
@@ -30,41 +31,13 @@ function getVols() {
 }
 
 function generateReadmeMd(vols) {
-  let mdText = `# 任听播客通讯
-![任听播客通讯](https://i.loli.net/2019/05/26/5cea3ae146d2d43108.png)
-
-任听播客通讯是一份<s>双周</s>（不定期）更新的邮件通讯，由 任平生、JW 分享他们这段时间听到有意思的播客内容。
-
-
-## 往期内容
-
-| 期数            | 发送日期      | 导语             |
-| :------------- | :--------- | :---------------- |
-`;
+  let mdText = fs.readFileSync('./src/template/readme/header.md', 'utf8');
 
   Object.keys(vols).forEach((key) => {
     mdText += `| [${vols[key].title}](${vols[key].path}) | ${vols[key].date} | ${vols[key].desc} |\n`;
   });
 
-  mdText += `
-
-## 订阅任听播客通讯
-
-- 邮件订阅： [https://letter.getpodcast.xyz/](https://letter.getpodcast.xyz/)
-- 公众号订阅：
-  ![](https://i.loli.net/2019/12/13/4SU2y7NWEgv5lwJ.jpg)
-
-
-## 赞赏支持
-赞赏时请备注你的Email
-
-- 微信：
-  ![](https://getpodcast.xyz/src/img/wx-zs.png)
-- 支付宝：
-  ![](https://getpodcast.xyz/src/img/zfb-zs.png)
-- Paypal: renpingshengx@gmail.com
-
-`;
+  mdText += fs.readFileSync('./src/template/readme/footer.md', 'utf8');
 
   return mdText;
 }
